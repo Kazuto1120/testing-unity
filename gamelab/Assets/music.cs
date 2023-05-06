a82ad1efb9cf39c659fd125e4269e6ff77b48255
Kazuto1120 <117380810+Kazuto1120@users.noreply.github.com>
2023-05-05 20:21:14 -0400

    add music
    add music

--- gamelab/Assets/music.cs
+++ gamelab/Assets/music.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class music : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public AudioSource audio;
+    public Slider slide;
+
+    private void Awake()
     {
-        
+        audio.volume = slide.value;
+    }
+    public void volume()
+    {
+        audio.volume = slide.value;
+    }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+
